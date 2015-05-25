541f835f66053e3f9a6aee3843d96c3228e292be
Abdalhadi <abdalhadi.m92@gmial.com>
2015-05-26 01:10:07 +0300

    add rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,9 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_review, only: [:edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!, except: [:index, :show]
 
   respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
 
   def new
     @review = Review.new
@@ -24,24 +16,29 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
+    @review.movie_id = @movie.id
 
     @review.save
-    respond_with(@review)
+    respond_with(@movie)
   end
 
   def update
     @review.update(review_params)
-    respond_with(@review)
+    respond_with(@movie)
   end
 
   def destroy
     @review.destroy
-    respond_with(@review)
+    respond_with(@movie)
   end
 
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     def review_params
